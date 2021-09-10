796975d8067536045d00b0f71a0f1f7e3d175f7f
Izzy <undermybrella@abimon.org>
2021-09-10 19:46:56 +1000

    Switch to curl over wget

--- update-borg-scripts.bash
+++ update-borg-scripts.bash
@@ -4,9 +4,9 @@ BACKUP_FILE='/usr/local/bin/backup-borg'
 RESTORE_FILE='/usr/local/bin/restore-borg'
 UPDATE_FILE='/usr/local/bin/update-borg-scripts'
 
-wget "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/backup-borg.bash" -o $BACKUP_FILE
-wget "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/restore-borg.bash" -o $RESTORE_FILE
-wget "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/update-borg-scripts.bash" -o $UPDATE_FILE
+curl "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/backup-borg.bash" > $BACKUP_FILE
+curl "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/restore-borg.bash" > $RESTORE_FILE
+curl "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/update-borg-scripts.bash" > $UPDATE_FILE
 
 BORG_REPO="$1"
 BORG_PASSPHRASE="$2"
