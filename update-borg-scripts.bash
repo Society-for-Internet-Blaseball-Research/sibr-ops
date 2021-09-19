4d82a700056a43b1799240f5e5336947c3471e7f
Izzy <undermybrella@abimon.org>
2021-09-19 22:34:52 +1000

    Prepare to use argbash for borg scripts

--- update-borg-scripts.bash
+++ update-borg-scripts.bash
@@ -4,8 +4,8 @@ BACKUP_FILE='/usr/local/bin/backup-borg'
 RESTORE_FILE='/usr/local/bin/restore-borg'
 UPDATE_FILE='/usr/local/bin/update-borg-scripts'
 
-curl "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/backup-borg.bash" > $BACKUP_FILE
-curl "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/restore-borg.bash" > $RESTORE_FILE
+curl "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/backup-borg.m4" > $BACKUP_FILE
+curl "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/restore-borg.m4" > $RESTORE_FILE
 curl "https://raw.githubusercontent.com/Society-for-Internet-Blaseball-Research/sibr-ops/master/update-borg-scripts.bash" > $UPDATE_FILE
 
 BORG_REPO="$1"
@@ -15,3 +15,9 @@ BORG_RSH="$3"
 PATTERN="$BORG_REPO" perl -pi.bak -e "s/export BORG_REPO=''/export BORG_REPO='\$ENV{PATTERN}'/" $BACKUP_FILE
 PATTERN="$BORG_PASSPHRASE" perl -pi.bak -e "s/export BORG_PASSPHRASE=''/export BORG_PASSPHRASE='\$ENV{PATTERN}'/" $BACKUP_FILE
 PATTERN="$BORG_RSH" perl -pi.bak -e "s/export BORG_RSH=''/export BORG_RSH='\$ENV{PATTERN}'/" $BACKUP_FILE
+
+argbash $BACKUP_FILE -o $BACKUP_FILE
+argbash $RESTORE_FILE -o $RESTORE_FILE
+
+argbash $BACKUP_FILE --type completion --strip all -o /etc/bash_completion.d/backup-borg.sh
+argbash $RESTORE_FILE --type completion --strip all -o /etc/bash_completion.d/restore-borg.sh
